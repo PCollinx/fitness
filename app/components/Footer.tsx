15d9c2f1ff99ce2d9e3663ef218980b7c037a9f1
PCollinx <paulcollins484@gmail.com>
2025-10-18 23:27:09 +0100

    changed the footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,8 +2,8 @@
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-900 text-white border-t border-gray-800">
-      <div className="mt-6 pt-4 md:px-4 border-t border-gray-800 flex flex-col md:flex-row justify-between items-center text-sm">
+    <footer className="bg-gray-900 text-white">
+      <div className=" pt-4 md:px-4 border-t border-gray-800 flex flex-col md:flex-row justify-between items-center text-sm">
         <p className="text-gray-500">
           &copy; {new Date().getFullYear()} FitTrack. All rights reserved.
         </p>
